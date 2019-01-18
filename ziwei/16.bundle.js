b59ae23e6d06330c6fce0b2511d57169f9dafa61
raylee <lijunray@me.com>
2019-01-18 04:10:06 -0500

    Repush mobile background3

--- ziwei/16.bundle.js
+++ ziwei/16.bundle.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{352:function(p,c,e){p.exports=e.p+"861c562ec9d5f7b34a70ceb1b324ee3c.jpg"}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{352:function(a,p,c){a.exports=c.p+"49ace78baea0fac1df1009aa879c5e93.jpg"}}]);
