9cb15e83a1254e080addaeefe6ec095fa5d5e818
raylee <lijunray@me.com>
2019-02-23 16:11:03 -0500

    Move data to separate constants file

--- src/client/Album/Album.react.js
+++ src/client/Album/Album.react.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const startText = [
-  '我想了很久到今天该做些什么',
-  '一直没有找到最满意的',
-  '后来有一天我在翻关于你的照片',
-  '......'
-];
-
 class Album extends Component {
   render() {
     console.log(this.props.photos);
---
+++ src/client/app.constants.js
@@ -0,0 +1,89 @@
+export const photoPaths = [
+  {
+    images: [import('../../public/assets/images/engagement/1.jpg')],
+    text: ''
+  },
+  {
+    images: [import('../../public/assets/images/engagement/2.jpg')],
+    text: ''
+  },
+  {
+    images: [import('../../public/assets/images/engagement/3.jpg')],
+    text: ''
+  },
+  {
+    images: [import('../../public/assets/images/engagement/4.jpg')],
+    text: ''
+  },
+  {
+    images: [import('../../public/assets/images/engagement/5.jpg')],
+    text: ''
+  },
+  {
+    images: [import('../../public/assets/images/engagement/6.jpg')],
+    text: ''
+  },
+  {
+    images: [import('../../public/assets/images/engagement/7.jpg')],
+    text: ''
+  },
+  {
+    images: [import('../../public/assets/images/engagement/8.jpg')],
+    text: ''
+  },
+  {
+    images: [import('../../public/assets/images/engagement/9.jpg')],
+    text: ''
+  },
+  {
+    images: [import('../../public/assets/images/engagement/10.jpg')],
+    text: ''
+  },
+  {
+    images: [import('../../public/assets/images/engagement/11.jpg')],
+    text: '你一定没有留意到我特意藏了一张...'
+  },
+  {
+    images: [],
+    text: '你一定没有留意到我特意藏了一张...'
+  },
+  {
+    images: [import('../../public/assets/images/engagement/willyoumarryme_ziwei.jpg')],
+    text: '它是这张...'
+  }
+];
+
+export const mobileBackgroundImages = [
+  import('../../public/assets/images/mobile/background1.jpg'),
+  import('../../public/assets/images/mobile/background2.jpg'),
+  import('../../public/assets/images/mobile/background3.jpg'),
+  import('../../public/assets/images/mobile/background4.jpg'),
+  import('../../public/assets/images/mobile/background5.jpg'),
+  import('../../public/assets/images/mobile/background6.jpg'),
+  import('../../public/assets/images/mobile/background7.jpg'),
+  import('../../public/assets/images/mobile/background8.jpg'),
+  import('../../public/assets/images/mobile/background9.jpg')
+];
+
+export const desktopBackgroundImages = [
+  import('../../public/assets/images/desktop/background1.jpg'),
+  import('../../public/assets/images/desktop/background2.jpg'),
+  import('../../public/assets/images/desktop/background3.jpg'),
+  import('../../public/assets/images/desktop/background4.jpg'),
+  import('../../public/assets/images/desktop/background5.jpg'),
+  import('../../public/assets/images/desktop/background6.jpg'),
+  import('../../public/assets/images/desktop/background7.jpg'),
+  import('../../public/assets/images/desktop/background8.jpg'),
+  import('../../public/assets/images/desktop/background9.jpg'),
+  import('../../public/assets/images/desktop/background10.jpg'),
+  import('../../public/assets/images/desktop/background11.jpg'),
+  import('../../public/assets/images/desktop/background12.jpg'),
+  import('../../public/assets/images/desktop/background13.jpg')
+];
+
+const startText = [
+  '我想了很久到今天该做些什么',
+  '一直没有找到最满意的',
+  '后来有一天我在翻关于你的照片',
+  '......'
+];
